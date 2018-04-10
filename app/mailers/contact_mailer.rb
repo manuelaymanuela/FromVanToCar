6bfb703a08f7db9893e1f815d6d9983a36580bd3
Manuela Solís <manuelaymanuela@hotmail.com>
2018-04-10 20:49:15 +0200

    seguir configurando mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,13 @@
 class ContactMailer < ApplicationMailer
     default from: '"Someone needs your vehicle" fromvantocar@gmail.com'
    
-    def contact_email(customer)
+    def contact_email(message, detail, date, vehicle, customer)
+      
+      @message = message
+      @detail = detail
+      @date = date
+      @vehicle = vehicle
       @customer = customer
-      @url = 'http://example.com/login'
       mail(to: @vehicle.user.email, subject: 'Welcome to FromVanToCar')
     end
   end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
     def contact_mail_preview
-        ContactMailer.contact_email(Vehicle.first)
+        ContactMailer.contact_email("Un mensaje para ti", "detail", "2018/04/01", Vehicle.first, User.first)
     end
 end
